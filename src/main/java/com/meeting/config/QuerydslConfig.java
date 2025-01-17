a0bdab466b14de6bbef50fadae2be7c567925cae
hyunjun <hyunjun5681@kakao.com>
2025-01-17 14:32:10 +0900

    Querydsl 설정 추가

---
+++ src/main/java/com/meeting/config/QuerydslConfig.java
@@ -0,0 +1,22 @@
+package com.meeting.config;
+
+import com.querydsl.jpa.JPQLTemplates;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(name = "spring.datasource.use", havingValue = "true", matchIfMissing = true)
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+    }
+}
