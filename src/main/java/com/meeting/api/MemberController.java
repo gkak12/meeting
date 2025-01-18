6b873eb23c77d111ba9d40b72f7a6ca2a3391015
hyunjun <hyunjun5681@kakao.com>
2025-01-18 19:24:01 +0900

    Member 이름 조회 예외 처리 추가

--- src/main/java/com/meeting/api/MemberController.java
+++ src/main/java/com/meeting/api/MemberController.java
@@ -3,6 +3,7 @@ package com.meeting.api;
 import com.meeting.domain.vo.MemberVo;
 import com.meeting.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +24,11 @@ public class MemberController {
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/name/{name}")
-    public ResponseEntity<MemberVo> findByMemberName(@PathVariable String name){
+    public ResponseEntity<MemberVo> findByMemberName(@PathVariable String name) throws Exception {
+        if(StringUtils.isBlank(name)){
+            throw new Exception("name is empty.");
+        }
+
         return ResponseEntity.ok(memberService.findByMemberName(name));
     }
 }
